cadad9443211b8c8c6121d54b88fbf32cca94fbd
Thayu <thayunathan@gmail.com>
2025-12-13 23:48:49 +0800

    add aws provider configuration

---
+++ infra/provider.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Project    = "infrastructure-action-tracker"
+      ManagedBy  = "terraform"
+      Repository = "thayu10/infrastructure-action-tracker"
+      Environment = var.environment
+    }
+  }
+}
